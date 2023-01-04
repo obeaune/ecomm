aa52122676e16bad56ff746368244160ed3737dc
Ana Arruda <linecora86@gmail.com>
2023-01-04 20:43:06 -0300

    create `usersList`

---
+++ account/src/use-case/createUserAccount.js
@@ -0,0 +1,23 @@
+const usersList = [
+    {
+        id: 1,
+        name: 'Finn, the Human',
+        email: 'grassSword17@ooo.com',
+        password: 'huntressWizardS2',
+        createdDate: '2022-01-14',
+    },
+    {
+        id: 2,
+        name: 'Jake, the Dog',
+        email: 'myBestFriendIsAHuman@ooo.com',
+        password: 'onionCheeseLobsterSoul99',
+        createdDate: '2022-11-11',
+    },
+    {
+        id: 3,
+        name: 'Marceline, the Vampire Queen',
+        email: 'bloodAndGuitars@ooo.com',
+        password: 'bonnie0and0me',
+        createdDate: '2000-01-01',
+    }
+];
