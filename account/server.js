d01c1eeef973cbfee414d9575863e1bfe5bc7b71
Ana Arruda <linecora86@gmail.com>
2023-02-08 16:24:54 -0300

    expose ports for account

--- account/server.js
+++ account/server.js
@@ -1,6 +1,6 @@
 import app from "./src/app.js";
 import {} from "dotenv/config";
 
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => console.log(`Server listen in http://localhost:${port}`));
--- account/src/config/dbConnect.js
+++ account/src/config/dbConnect.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 mongoose.set("strictQuery", true);
 
-mongoose.connect("mongodb+srv://admin:sin123@ecomm.emvw6uk.mongodb.net/ecomm");
+// mongoose.connect("mongodb+srv://admin:sin123@ecomm.emvw6uk.mongodb.net/ecomm");
+mongoose.connect("mongodb://admin:secret@mongodb:27017/ecomm-account?authSource=admin");
 
 const db = mongoose.connection;
 
