b1cd684e8a184be6c031bc20b7c4d50d2403ec3b
Ana Arruda <linecora86@gmail.com>
2023-02-13 17:49:54 -0300

    add validation for /patch

--- finance/src/routes/paymentsRoute.js
+++ finance/src/routes/paymentsRoute.js
@@ -2,12 +2,14 @@ const { Router } = require("express");
 
 const PaymentController = require("../controllers/PaymentController");
 
+const preValidation = require("../validation/validatePaymentStatus");
+
 const router = Router();
 
 router
     .get("/payments", PaymentController.getPayments)
     .get("/payments/:id", PaymentController.getPaymentById)
     .post("/payments", PaymentController.createPayment)
-    .patch("/payments/status/:id", PaymentController.updatePaymentStatus);
+    .patch("/payments/status/:id", preValidation.verifyStatusAndID, PaymentController.updatePaymentStatus);
 
 module.exports = router;
