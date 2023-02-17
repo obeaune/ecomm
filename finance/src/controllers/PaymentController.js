9e1f0cfc523b2bf3986fb78e687368c03a9e6dbb
Ana Arruda <linecora86@gmail.com>
2023-02-17 11:49:07 -0300

    add links/hateoas logic

--- finance/src/controllers/PaymentController.js
+++ finance/src/controllers/PaymentController.js
@@ -15,7 +15,25 @@ class PaymentController {
         const infoPayment = req.body;
         try {
             const { id, status } = await db.Payments.create(infoPayment);
-            return res.status(201).location(`/payments/${id}`).json({ message: `Payment: ${id}. Status: ${status}`});
+            const links = [
+                { 
+                    method: "GET",
+                    rel: "self information",
+                    href: `http://localhost:3003/payments/${id}`
+                },
+                { 
+                    method: "PATCH",
+                    rel: "change status to confirm",
+                    href: `http://localhost:3003/payments/status/${id}`
+                },
+                { 
+                    method: "PATCH",
+                    rel: "change status to cancel",
+                    href: `http://localhost:3003/payments/status/${id}`
+                },
+
+            ];
+            return res.status(201).location(`/payments/${id}`).json({ id, status, links });
         } catch(err) {
             return res.status(500).json(err.message);
         }
@@ -35,8 +53,9 @@ class PaymentController {
         const { id } = req.params;
         const { status } = req.body;
         try {
+            if(status === undefined) return res.status(400).json({ message: "You need to pass the new status in the request body."});
             await db.Payments.update({ status }, { where: { id: +id } });
-            return res.status(200).json({ message: `Status of payment -${id}- was successfully updated to -${status}-.` });
+            return res.status(200).json({ message: `Status of payment -${id}- was successfully updated for -${status}-.` });
         } catch(err) {
             return res.status(500).json(err.message);
         }
--- finance/src/validation/validatePaymentStatus.js
+++ finance/src/validation/validatePaymentStatus.js
@@ -2,12 +2,13 @@ const db = require("../models");
 
 const verifyStatusAndID = async (req, res, next) => {
     const { id } = req.params;
+    const { status } = req.body;
     try {
         const paymentExists = await db.Payments.findOne({ where: { id: +id } });
         if (!paymentExists) {
             return res.status(404).json({ message: "User not found. Try again with a valid ID." });  
         } else if (paymentExists.status === "CONFIRMED" || paymentExists.status === "CANCELED") {
-            return res.status(405).json({ message: "Status can't be changed." });
+            return res.status(405).json({ message: `Status -${paymentExists.status}- can't be changed.` });
         } else {
             next();
         }
