9f232a67c1ff09d065f0154b2430af162f5f63e2
Ana Arruda <linecora86@gmail.com>
2023-02-22 09:45:38 -0300

    change some details in `verifyStatusAndID`

--- finance/src/validation/validatePaymentStatus.js
+++ finance/src/validation/validatePaymentStatus.js
@@ -1,13 +1,14 @@
 const db = require("../models");
 
+const availableStatus = ["CONFIRMED", "CANCELED"];
+
 const verifyStatusAndID = async (req, res, next) => {
     const { id } = req.params;
-    const { status } = req.body;
     try {
         const paymentExists = await db.Payments.findOne({ where: { id: +id } });
         if (!paymentExists) {
-            return res.status(404).json({ message: "User not found. Try again with a valid ID." });  
-        } else if (paymentExists.status === "CONFIRMED" || paymentExists.status === "CANCELED") {
+            return res.status(404).json({ message: "User not found. Try again with a valid ID." });
+        } else if (availableStatus.includes(paymentExists.status.toUpperCase())) {
             return res.status(405).json({ message: `Status -${paymentExists.status}- can't be changed.` });
         } else {
             next();
